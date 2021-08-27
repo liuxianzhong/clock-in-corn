c8e1cda03705fbd60d18d5b6314f792ea0739e6e
fixedbug <2330996055@qq.com>
2021-08-27 11:25:06 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-CITY=Shenzhen
+CITY=Guiyang
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
